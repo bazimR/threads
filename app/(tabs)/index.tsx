4013d3be3907cb079d25d9437880d56d86d0aa1d
bazimR <rishal@htsqatar.com>
2023-09-09 17:45:16 +0530

    threads

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,20 @@
-import { StyleSheet } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
-
+import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
+import AnimatedLottieView from "lottie-react-native";
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <AnimatedLottieView
+          source={require("../../assets/animations/threadsOg.json")}
+          loop
+          style={{ width: 90, height: 100, flex: 1 }}
+          autoPlay
+          resizeMode="cover"
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
